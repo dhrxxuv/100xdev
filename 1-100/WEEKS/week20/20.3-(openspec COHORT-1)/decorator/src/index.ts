438569fbe080f75ca8c2e343d480550d92ecad85
dhrxxuv <Dhruvneekhra01@gmail.com>
2025-11-15 16:26:45 +0530

    decorators

---
+++ 1-100/WEEKS/week20/20.3-(openspec COHORT-1)/decorator/src/index.ts
@@ -0,0 +1,30 @@
+import { once } from "helpful-decorators";
+
+class DateClass {
+    private timezone: string;
+
+    constructor(timezone: string){
+        this.timezone = timezone
+    }
+    @once
+    getTime(){
+        const res = new Date()
+        res.getTime()
+        if(this.timezone==="IST"){
+            return res.toLocaleString("en-US",{timeZone:"Asia/Kolkata"})
+        }
+        return res
+    }
+}
+
+const dateObject = new DateClass("")
+const response = dateObject.getTime()
+dateObject.getTime()
+dateObject.getTime()
+dateObject.getTime()
+dateObject.getTime()
+dateObject.getTime()
+dateObject.getTime()
+dateObject.getTime()
+dateObject.getTime()
+console.log(response)
